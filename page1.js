c96421e1c74af685922efa3f4cf2272d08240e90
Jitchangdarr <jitchangdar7@gmail.com>
2024-12-29 14:22:08 +0530

    folder add

---
+++ page1.js
@@ -0,0 +1,36 @@
+// var x=2;
+// var a=10;
+// function b(){
+//     var x=10;
+// }
+// console.log(x)
+// console.log(a)
+// console.log(window.a)
+// console.log(this.a)
+
+//  a=10;
+//  b=12;
+// function a(){
+//     function b(){
+       
+//     }
+// }
+// console.log(a);
+// console.log(b);
+console.log(a)
+  var a=40;
+  if(a === undefined){
+    console.log("jit");
+  }
+  else{
+    console.log("subho");
+  }
+
+
+  
+// if(a === undefined){
+//     console.log("my name is jit..")
+// }else{
+// console.log("cfvbb");
+// }
+
